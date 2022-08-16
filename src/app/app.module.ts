64676eb6cc59686e6573171995ea09a8a34e11ed
hieudao10110 <daovanhieu10110@gmail.com>
2022-08-16 09:11:30 +0700

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { MainHomeComponent } from './main-home/main-home.component';
+import { MainServicesComponent } from './main-services/main-services.component';
+import { MainAboutComponent } from './main-about/main-about.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    MainHomeComponent,
+    MainServicesComponent,
+    MainAboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: MainHomeComponent },
+      { path: 'services', component: MainServicesComponent },
+      { path: 'about', component: MainAboutComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
